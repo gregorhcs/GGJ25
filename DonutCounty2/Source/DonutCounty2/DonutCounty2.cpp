4ec8818e9a9f3ee77d41af6c20a7fd9f6c5978be
Gregor Sönnichsen <gregor.soennichsen@outlook.com>
2025-01-24 22:24:03 +0100

    Add empty UE project

---
+++ DonutCounty2/Source/DonutCounty2/DonutCounty2.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DonutCounty2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DonutCounty2, "DonutCounty2" );
---
+++ DonutCounty2/Source/DonutCounty2Editor.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class DonutCounty2EditorTarget : TargetRules
+{
+	public DonutCounty2EditorTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
+		ExtraModuleNames.Add("DonutCounty2");
+	}
+}
